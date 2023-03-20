db2957ed18179b8219e71b79643d57c82b1c4b81
akumasan <ldlvvna@gmail.com>
2023-03-20 15:42:39 -0700

    week-2

---
+++ week-2/w2-mon-declare-values-keys.js
@@ -0,0 +1,21 @@
+/*
+
+
+
+*/
+
+
+
+
+const obj = {};
+
+obj.firstKey = 'firstValue';
+obj.numeric = '2';
+obj.boolean = 'false';
+obj.object = '{}';
+
+
+console.log(obj['firstKey']);       // firstValue
+console.log(obj['numeric']);        // 2
+console.log(obj['boolean']);        // false
+console.log(obj['object']);         // {}
