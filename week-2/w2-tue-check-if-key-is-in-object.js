33424f5d7132fb524e80213b6326f7ac69fd7d9e
akumasan <ldlvvna@gmail.com>
2023-03-21 13:12:10 -0700

    check-key-in-obj

---
+++ week-2/w2-tue-check-if-key-is-in-object.js
@@ -0,0 +1,23 @@
+/*
+
+Check if key is in object
+Write a function includedInObject that accepts an object, obj, and a key
+and returns true if that key exists on the object or false otherwise.
+
+*/
+
+const obj = {
+    first: "1",
+    second: 2,
+    third: "three"
+}
+
+const str = "first";
+
+const variableAsKey = (obj, str) => {
+    console.log(obj[str]);
+    return obj[str];
+}
+
+variableAsKey(obj, str);                // "1" (looks like 1 in terminal)
+variableAsKey(obj, "second");           // 2
