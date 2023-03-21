1d7a74457ff91ca1b7531a2698648baf96e41149
akumasan <ldlvvna@gmail.com>
2023-03-21 08:30:41 -0700

    values-as-keys

--- week-2/w2-mon-declare-values-keys.js
+++ week-2/w2-mon-declare-values-keys.js
@@ -1,12 +1,10 @@
 /*
 
 Declare Keys/Values
-Given the following declaration of an object, obj below, declare values so
+Given the following declasration of an object, obj below, declare values so
 that the print statements output what is expected. Try using both dot and bracket notation.
-
+s
 */
-
-
 
 
 const obj = {};
