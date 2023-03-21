ca88b855af8b283c26b18707a576e17a5c1bc843
akumasan <ldlvvna@gmail.com>
2023-03-20 17:25:08 -0700

    free-code

---
+++ week-2/free-code.js
@@ -0,0 +1,41 @@
+/*
+
+console.log('run'[-1]);
+
+console.log('run'['run'.length - 1]);
+
+console.log('run'[3]);
+
+console.log('bootcamp'.indexOf('x'));
+
+console.log('bootcamp'.indexOf('t'));
+
+console.log('run'.length);
+
+console.log('run'[2]);
+
+console.log('bootcamp'.indexOf('o'));
+
+console.log('bootcamp'.indexOf('camp'));
+
+
+
+function addThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+  }
+  
+  console.log(addThreeNumbers(1, 2)); // => ???
+
+  
+
+  function isDivisibleByHundred(num) {
+    return num % 100 === 0;
+  }
+
+  console.log(isDivisibleByHundred(225));
+
+  */
+
+  let x = 1;
+let y = 2;
+console.log(x * 3 / y + 7 % 4);
