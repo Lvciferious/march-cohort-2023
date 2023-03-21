c9577c9be8f826b711f428e6684f16880c477555
akumasan <ldlvvna@gmail.com>
2023-03-21 08:59:37 -0700

    iterate-through-obj

--- week-2/w2-mon-array-of-objects.js
+++ week-2/w2-mon-array-of-objects.js
@@ -1,3 +1,12 @@
+/*
+
+Array of Objects
+In this practice, you will practice navigating objects that are nested within
+an array. Given the below array, write a function printNames that prints the
+name of every object
+
+*/
+
 //GAMEPLAN
 //for loop single pringle
 
@@ -33,6 +42,7 @@ printNames(users)                   // Gerald Winnie Peter
 
 //dot notation
 const printNames2 = users2 => {
+    // Write your solution here.
     for (let i = 0; i < users2.length; i++) {
         let currentName2 = users2[i];
         console.log(currentName2.name);
